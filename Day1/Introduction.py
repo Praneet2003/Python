85fee54bcedad8553ab4e8c84e8847841dd9f813
Praneet2003 <rajpraneet4@gmail.com>
2024-04-18 22:26:02 +0530

    Introduction to pythonand it's features

---
+++ Day1/Introduction.py
@@ -0,0 +1,18 @@
+# Python:-
+# python is a dynamically typed object orinted programming language.
+# It was developed by Guido Van Rossum in 1991.
+# Features of Python:-
+# 1:) Simple Programming language
+# 2:) Platform independent.(code in mac os , you can use it on windows as well)
+# 3:) It has Rich Library.
+# 4:) Interpreted language.
+# 5:) Very easy to link with other programming languages.
+
+#Application of Python:-
+# 1:) Used for Data Visualization to create plots and graphical representation.
+# 2:) It help us in Data Analysis and understand raw data for insights and trends.
+# 3:) It is used in AI and Machine Learning. 
+# 4:) It is used to create web Applications.
+# 5:) It is used to handel database.
+# 6:) It is also used in Buisness and accounting to perform Complex mathematical operations along with Quatative and Qualitavite Analysis.
+
