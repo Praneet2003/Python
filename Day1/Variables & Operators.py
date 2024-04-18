d965447a7f550b534b7445800cc1bf1b31693ebd
Praneet2003 <rajpraneet4@gmail.com>
2024-04-18 22:28:09 +0530

    Variables in Python

---
+++ Day1/Variables & Operators.py
@@ -0,0 +1,52 @@
+
+# Variables:-
+#variables :-
+# It Stores the data value, at a specific memay location
+# Eg:- 
+# G = 127406
+# b = "planet"
+# C = false
+# d = None.
+
+#Data types :-
+# Data types specifies the type of value Which a variable stores.
+# we can find the datatype of any variable Using type().
+# Eg:-
+a = 78
+print("The datatype of a 14", type(a))
+
+# 1:)Numeric data: int, float, Complex int 3,4, 9, 100 che fhat 28, 962 complex 2172, SHOL
+# 2:)Text data; str= "Hello world"
+# 3:)Boolean data. True, false.
+# 4:)sequence data:- list, tuple.
+
+
+# list-
+# It is an ordered collection of data sepeated by looms, and enclosed within square brackets []
+# eg:-
+list1 = [1, 23, "Hello", [1, 26, 14.67,"hello world"],"Bye"]
+print(list1)
+# * list are mutable (Changeable)
+
+# Tuple:-
+# it is also ordered collection of dada seperated by commas, and enclosed within parenthesis ()
+tuple1 = ("ust", 1.5, ("Hello", 1, "world"))
+print(tuple1)
+# Tuples au immutable (not-changeable)
+
+# Mapped data:
+# 1:) Dictonary
+# Ditory in an unordered colletion of date Which consist of Key:value pairs enclosed within curly braces {}.
+# eg- 
+dict1 = {"name": "planet", "age": 20, "CanVote": True}
+print(dict1)
+# It is also mutable
+
+# 2:)set 
+# Set is an ordered collection of data seperated by commas within curly braces {}
+#Eg:-
+Set1 = {"Apple","Banana","Cherry",True,29,11}
+print(Set1)
+# Set are immutable, but you can delete or add new items to it.
+# Duplicates are not allowed,i.e they are ignored by interpreter.
+ 
