f2bb6e545d88a5d6d6c436dde675d2330200a1ec
Praneet2003 <rajpraneet4@gmail.com>
2024-04-18 22:29:20 +0530

    Modules in Python

--- Day1/Modules.py
+++ Day1/Modules.py
@@ -1,2 +1,11 @@
 # Modules:-
+# Modules are similar to library.
 # It is Basically used to borrow SOmeone else's code.
+# Modules ae of two type:-
+# 1:) Inbuilt Module:-
+# It comes or shipped with python interpreter.
+# 2:) Explicit Module:-
+# It is installed by user in their systems using pip
+# PIP Command:-
+# Syntax:-
+# pip install <module_name>
