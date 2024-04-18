aed72ff1c1d27495460c733e04af8ebb2e1d6c04
Praneet2003 <rajpraneet4@gmail.com>
2024-04-18 22:27:28 +0530

    Print statement and its attributes

---
+++ Day1/print statement,variables,operator.py
@@ -0,0 +1,28 @@
+# print statement
+# syntax print(<write what to print>)
+#eg:-
+print("Hello world")
+# Escape Sequence Characters:-
+# 1:) \n => for new line Character.
+# eg:- 
+print("Hello World\n")
+print("I am in next line")
+# 2:) " 'xyz' " => 
+print("Hey i am 'Praneet' an i am 20 years old.")
+# 3:) " "xyz" " =>
+print("Hey my second name is \"Anshu\".")
+
+# Some more Attributes in print Statements:-
+# Attributes under print statement:-
+# 1:) seperator (sep="") =>
+# To specifies how to seperate the ohjata, If there & more than, Default ta
+# eg 
+print ("Hey", 6, 7, sep = '~')
+# 2:)end =>
+# It specifies that what to print at the end. Default is '\n'
+# eg-
+print("praneet", 23, 11, sep = 'N', end = '009\n') 
+print("Hello")
+
+# 3:) file:- An object with a write method. default is syrstdout
+# 4:) objects: Any object, as many as you like it will be converted to string before Printed 
