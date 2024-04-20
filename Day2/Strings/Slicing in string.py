bf0c448c37e47965ee9530bb35e95c394fd569c7
Praneet2003 <rajpraneet4@gmail.com>
2024-04-20 19:25:51 +0530

    Slicing of string

---
+++ Day2/Strings/Slicing in string.py
@@ -0,0 +1,17 @@
+str1 = "papaya"
+print(len(str1))
+print(str1[0:4])# 0 is included but 4 is not included.
+# slicing in String:-
+# Synatx:
+# <str_name>[stsrt_index: end_index]
+# Eg:-
+fav = """Hey mehul what is your favourite food?
+Yes! praneet ,My Favorite fruit is Mango."""
+# length function in string len(<string_name>)
+len = len(fav)
+print(len)
+print(fav[39:81])
+print(fav[:len])
+fruit = "Mango"
+print(fruit[-1: -3])
+
