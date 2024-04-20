4aa65186b7f1e36cab954cfbf7b3f240034a6480
Praneet2003 <rajpraneet4@gmail.com>
2024-04-20 19:23:34 +0530

    Type Conversion

---
+++ Day2/Typecasting in python.py
@@ -0,0 +1,22 @@
+# Conversion of datatype into another datatype.
+# Python supports wide variety of function,method,like:- int(),float(),str(),tuple(),ord(),hex(),oct(),set(),list(),dict() etc
+# there are two tyoe of typeconversion:-
+# 1:) Explicit TypeCosting:-(It is done by user by using the above functions and methods)
+# 2:) Implicit TypeCasting:- it is done by interpreter which converts a smaller data into a larger data type
+
+# Explicit TypeCasting:-
+# type conversion done via developer or programmers.
+# Eg:-
+string = "15"
+num = 7
+string_num = int(string)
+sum = num + string_num
+print("The Sume after type Conversion is :",sum) 
+
+#Implicit TypeCasting:-
+a =1.9
+b=8
+sum = a+b
+print("\n")
+print(a+b)
+
