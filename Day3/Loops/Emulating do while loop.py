6d14dad174d553f5a65b37e4ca31a2199621e147
Praneet2003 <rajpraneet4@gmail.com>
2024-04-21 22:27:29 +0530

    Emulating do while loop in python

---
+++ Day3/Loops/Emulating do while loop.py
@@ -0,0 +1,9 @@
+# Emulating do while loop in python using infite while loop
+# Do While Loop:-
+# it Executes Atleast once even if the conditions are not true.
+i =0 
+while True:# infinite while loop.
+    print(i)
+    i=i+1
+    if(i%50==0):
+        break
