0763bef6740e87cd72611db13666df15cfb6e9d1
Praneet2003 <rajpraneet4@gmail.com>
2024-04-21 22:26:23 +0530

    While loop

---
+++ Day3/Loops/while loop.py
@@ -0,0 +1,9 @@
+i =0
+while(i<100):
+    print(i+2,end=" ")
+    i=i+2
+j = int(input("enter the value of j: "))
+while (j<=38):
+    j = int(input("enter tha value of j: "))
+    print(j)
+print("Out of the loop")
