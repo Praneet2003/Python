9f470dde592d85d8ff944f1b16a888c14e6f6b12
Praneet2003 <rajpraneet4@gmail.com>
2024-04-23 22:05:47 +0530

    Default arguments

---
+++ Day3/functions/default arguments.py
@@ -0,0 +1,8 @@
+# Default Arguments:- Providing values during function creation.
+#Eg:-
+def average(a =9, b =3):
+    print("The average is : ",(a+b)/2)
+# we can also provide a default value while creating a function,
+# this way the function assumes that a value is not provided in the function call.
+print(average(10,30))
+
